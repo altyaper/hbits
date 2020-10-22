7936d9298388e5b7a32774221c67f889f8094b22
jorge.chavez <jorge.chavez@points.com>
2020-10-22 01:41:17 -0700

    Add page with habits

--- lib/hbits_web/controllers/page_controller.ex
+++ lib/hbits_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule HbitsWeb.PageController do
   use HbitsWeb, :controller
 
+  alias Hbits.Habits
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    habits = Habits.list_habits()
+    render(conn, "index.html", habits: habits)
   end
 end
